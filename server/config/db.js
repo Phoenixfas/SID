826a8f7760f506ba1729fbc92627bbff5ddf2a63
Fasika Dagnachew <72998972+Phoenixfas@users.noreply.github.com>
2022-08-10 23:10:40 +0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect("mongodb+srv://Phoenixfas:3867Ff1515@cluster0.u9pcvp8.mongodb.net/sid_db?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
